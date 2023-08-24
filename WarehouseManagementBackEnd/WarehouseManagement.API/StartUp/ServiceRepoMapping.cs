50f81c357a5556d059c097187ccbc8b33d1c8108
Kiệt Nguyễn Anh <anhkietlop10a3hvs@gmail.com>
2023-08-24 13:54:03 +0700

    fsd

---
+++ WarehouseManagementBackEnd/WarehouseManagement.API/StartUp/ServiceRepoMapping.cs
@@ -0,0 +1,26 @@
+﻿using WarehouseManagement.DAL.Contract;
+using WarehouseManagement.DAL.Implementation;
+using WarehouseManagement.Service.Contract;
+using WarehouseManagement.Service.Implementation;
+
+namespace WarehouseManagement.API.StartUp
+{
+    public class ServiceRepoMapping
+    {
+        public ServiceRepoMapping() { }
+
+        public void Mapping(WebApplicationBuilder builder)
+        {
+            #region Service Mapping
+            builder.Services.AddScoped<IAuthService, AuthService>();
+            builder.Services.AddScoped<IWarehouseService, WarehouseService>();
+            builder.Services.AddScoped<IUserManagementService, UserManagamentService>();
+            #endregion Service Mapping
+
+            #region Repository Mapping
+            builder.Services.AddScoped<IWarehouseRepository, WarehouseRepository>();
+            builder.Services.AddScoped<IUserManagementRepository, UserManagementRepository>();
+            #endregion Repository Mapping
+        }
+    }
+}
